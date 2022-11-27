d3c8a8286ff1b55ccc5cef50d4b28ff4e31ebd7d
Bilal Ahmad <bilal.dourani@outlook.com>
2022-11-27 16:07:30 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Reddit() {
+  const [posts, setPosts] = useState ([]);
+
+React.useEffect(() => {
+  axios.get ('https://www.reddit.com/r/reactjs.json')
+  .then(res => {
+    const newPosts=res.data.data.children.map(obj => obj.data);
+    setPosts(newPosts);
+  });
+}, []);
+
+return (
+  <div>
+    <h1>/r/reactjs</h1>
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Reddit />, document.getElementById('root'));
+}
